fc3a15681e93c6e73da4e1f206a9c26e06075166
vietthang09 <vthcvn@gmail.com>
2023-12-01 07:46:05 +0700

    feat: update

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import pandas as pd
 class LMS:
     def add_new_book(self, data):
         with open('books.txt', 'a') as f:
-            new_book_data = '\n' + data[0] + "," + data[1] + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5] + ",available"
+            new_book_data = data[0] + "," + data[1] + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5] + ",available" + '\n'
             f.write(new_book_data)
         return True
     
@@ -39,19 +39,6 @@ class LMS:
         for line in lines:
             data.append(line.strip().split(','))
         return data
-
-    
-    def miscellaneous_books(self):
-        self.cur.execute("SELECT * FROM books WHERE status = 'miscellaneous'")
-        return self.cur.fetchall()
-    
-    def view_issued_book(self,id):
-        self.cur.execute("SELECT * FROM issued_book WHERE book_id = ? and is_miscellaneous = ?", (id,0))
-        return self.cur.fetchone()
-    
-    def view_student(self,id):
-        self.cur.execute("SELECT * FROM student WHERE id = ?", (id,))
-        return self.cur.fetchone()
     
     def all_book_id(self):
         book_ids = []
@@ -71,40 +58,6 @@ class LMS:
                 student_ids.append(student_id)
         return student_ids
     
-    def issue_book(self,data):
-        sql = '''INSERT INTO issued_book (book_id,issued_to,issued_on,expired_on)
-            VALUES(?,?,?,?) '''
-        self.cur.execute(sql, data)
-        self.conn.commit()
-        return self.cur.lastrowid
-    
-    def delete_issued_book(self):
-        try:
-            sql = 'DELETE FROM issued_book'
-            self.cur.execute(sql)
-            self.conn.commit()
-            return "deleted"
-        except:
-            return "error"
-    
-    def all_issued_book_id(self):
-        self.cur.execute("SELECT book_id FROM issued_book WHERE is_miscellaneous = ?",(0,))
-        return self.cur.fetchall()
-    
-    def return_book(self,book_id):
-        try:
-            sql = 'DELETE FROM issued_book WHERE book_id=?'
-            self.cur.execute(sql, (book_id,))
-            self.conn.commit()
-            return "returned"
-        except:
-            return "error"
-    
-    def update_book_status(self,book_id,status):
-        sql = '''UPDATE books SET status = ? WHERE book_id = ?'''
-        self.cur.execute(sql,(status,book_id,))
-        self.conn.commit()
-    
     def select_book_status(self,book_id):
         with open('books.txt', 'r') as f:
             lines = f.readlines()
@@ -113,37 +66,13 @@ class LMS:
             if line.strip().split(',')[0] == str(book_id):
                 return line.strip().split(',')[6]
     
-    def select_issued_book_det(self,book_id):
-        self.cur.execute("SELECT * FROM issued_book WHERE book_id=?", (book_id,))
-        return self.cur.fetchone()
-    
     def select_book_detail(self,book_id):
         with open('books.txt', 'r') as f:
             for line in f:
                 data = line.strip().split(',')
                 if data[0] == str(book_id):
                     return data
-    
-    def all_available_book(self):
-        sql="SELECT book_id, book_name, book_author, book_edition, book_price FROM books WHERE status = 'available'"
-        return (sql,self.conn)
-    
-    def all_issued_book(self):
-        sql="SELECT book_id, book_name, book_author, book_edition, book_price FROM books WHERE status = 'issued'"
-        return (sql,self.conn)
-    
-    def all_books(self):
-        sql="SELECT book_id, book_name, book_author, book_edition, book_price FROM books WHERE status = 'available' or status = 'issued'"
-        return (sql,self.conn)
-    
-    def fine_detail(self):
-        sql="SELECT * FROM fine_details"
-        return (sql,self.conn)
-    
-    def move_to_miscellaneous(self,id):
-        sql = '''UPDATE issued_book SET is_miscellaneous = ? WHERE book_id = ?'''
-        self.cur.execute(sql,(1,id,))
-        self.conn.commit()
+
     
     def update_book_details(self,book_id, data):
         with open('books.txt', 'r') as f:
@@ -156,11 +85,3 @@ class LMS:
 
         with open('books.txt', 'w') as f:
             f.writelines(lines)
-
-    
-    def save_fine_detail(self,data):
-        sql = '''INSERT INTO fine_details(book_id,student_id,issued_on,returned_date,total_fine,no_of_day)
-            VALUES(?,?,?,?,?,?)'''
-        self.cur.execute(sql, data)
-        self.conn.commit()
-        return self.cur.lastrowid
